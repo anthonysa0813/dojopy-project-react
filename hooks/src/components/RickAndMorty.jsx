5932ab2d30bbc4925f9741016ab088cd3b51dcfd
anthony sanchez <anthonysa0813@gmail.com>
2022-03-23 20:11:55 -0500

    v1: rickandmorty app

--- hooks/src/components/RickAndMorty.jsx
+++ hooks/src/components/RickAndMorty.jsx
@@ -3,15 +3,13 @@ import CardCaracter from "./CardCaracter";
 import "./rickandmorty.css";
 
 const RickAndMorty = () => {
+  const [value, setValue] = useState("");
+  const [darkMode, setDarkMode] = useState(false);
+
   const [dataApi, setDataApi] = useState([]);
 
-  // useEffect(() => {
-  //   console.log("me ejecuté!!!");
-  //   getApi();
-  // });
-
+  // este hook se encarga de hacer la petición a la api cuando se recarga la página por primera vez
   useEffect(() => {
-    console.log(dataApi);
     getApi();
   }, []);
 
@@ -21,24 +19,58 @@ const RickAndMorty = () => {
     setDataApi(data.results); // []
   };
 
+  const getAPiWithCaracter = async () => {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?name=${value}`
+    );
+    const data = await response.json();
+    setDataApi(data.results); // []
+  };
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    // con e.preventDefault prevenimos el envio y el refresh de la página
+    e.preventDefault();
+
+    // validaciones de formulario
+    if (value.trim() === "") {
+      console.log("Campo vacío!!!!");
+      // el return va a ser que se termine la función
+      return;
+    }
+    console.log("solicitando a la api.... :D");
+    getAPiWithCaracter();
+  };
+
+  const changeMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <>
+    <div
+      className={
+        darkMode ? "globalContainer darkMode" : "globalContainer lightMode"
+      }
+    >
       <div className="appContainer p-5">
         <div className="wrapper">
-          <div className="controlsContainer">
+          <form className="controlsContainer" onSubmit={handleSubmit}>
             <input
               type="text"
               className="form-control w-50"
               placeholder="Escribe un personaje"
+              onChange={handleChange}
             />
-            <button
-              className="btn btn-primary mx-4"
-              onClick={() => console.log("me clickeaste!!!")}
-            >
+            <button className="btn btn-primary mx-4" type="submit">
               Buscar
             </button>
-            <button className="btn btn-dark">Dark mode</button>
-          </div>
+            <button className="btn btn-dark" onClick={changeMode}>
+              Dark mode
+            </button>
+          </form>
         </div>
       </div>
       <main>
@@ -51,7 +83,7 @@ const RickAndMorty = () => {
           </div>
         </div>
       </main>
-    </>
+    </div>
   );
 };
 
